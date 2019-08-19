e3859140c422a783dfef3a171ed6265600856de2
Jason <jasondien@gmail.com>
2019-08-19 12:51:00 -0700

    Added story text file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import protagonist.MC;
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
         MC hektor = MC.getMC();
@@ -7,6 +9,43 @@ public class Main {
         System.out.println(hektor.mp);
         System.out.println(hektor.energy);
         System.out.println(hektor.exp);
+
+        // The name of the file to open.
+        String fileName = "story.txt";
+        File file = new File(fileName);
+        System.out.println(file.exists());
+
+        // This will reference one line at a time
+        String line = null;
+
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader =
+                    new FileReader(fileName);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader =
+                    new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            // Always close files.
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file '" +
+                            fileName + "'");
+        }
+        catch(IOException ex) {
+            System.out.println(
+                    "Error reading file '"
+                            + fileName + "'");
+            // Or we could just do this:
+            // ex.printStackTrace();
+        }
     }
 }
 
